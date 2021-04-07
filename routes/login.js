d8f9b043ebcbdeb2af8f5f97892808b6134f1bfe
https://gitlab.com/60070046/web_project.git <60070046@kmitl.ac.th>
2021-04-07 16:35:39 +0700

    ลบ ผิดอัน

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const login = require("../controllers/login");
-
-router.route('/')
-    .post(login.login)
-
-
-module.exports = router;
